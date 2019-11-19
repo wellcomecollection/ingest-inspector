f155ee710a29454d26f48df1d0c79950ac7475f1
Glitch (hello-flask) <none>
2019-11-19 08:06:07 +0000

    🚲🏚 Checkpoint

--- server.py
+++ server.py
@@ -1,119 +1,58 @@
-# -*- encoding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import datetime as dt
+#       _                              
+#      | |                             
+#    __| |_ __ ___  __ _ _ __ ___  ___ 
+#   / _` | '__/ _ \/ _` | '_ ` _ \/ __|
+#  | (_| | | |  __/ (_| | | | | | \__ \
+#   \__,_|_|  \___|\__,_|_| |_| |_|___/ .
+#
+# A 'Fog Creek'–inspired demo by Kenneth Reitz™
+
 import os
-
 from flask import Flask, request, render_template, jsonify
-from wellcome_storage_service import IngestNotFound, StorageServiceClient
 
-app = Flask(__name__)
+# Support for gomix's 'front-end' and 'back-end' UI.
+app = Flask(__name__, static_folder='public', template_folder='views')
 
-prod_api = StorageServiceClient(
-    api_url="https://api.wellcomecollection.org/storage/v1",
-    client_id=os.environ["CLIENT_ID"],
-    client_secret=os.environ["CLIENT_SECRET"],
-    token_url="https://auth.wellcomecollection.org/oauth2/token"
-)
+# Set the app secret key from the secret environment variables.
+app.secret = os.environ.get('SECRET')
 
-staging_api = StorageServiceClient(
-    api_url="https://api-stage.wellcomecollection.org/storage/v1",
-    client_id=os.environ["CLIENT_ID"],
-    client_secret=os.environ["CLIENT_SECRET"],
-    token_url="https://auth.wellcomecollection.org/oauth2/token"
-)
+# Dream database. Store dreams in memory for now. 
+DREAMS = ['Python. Python, everywhere.']
 
 
-@app.route("/")
-def index_page():
-    return render_template("index.html", title="Look up an ingest")
+@app.after_request
+def apply_kr_hello(response):
+    """Adds some headers to all responses."""
+  
+    # Made by Kenneth Reitz. 
+    if 'MADE_BY' in os.environ:
+        response.headers["X-Was-Here"] = os.environ.get('MADE_BY')
+    
+    # Powered by Flask. 
+    response.headers["X-Powered-By"] = os.environ.get('POWERED_BY')
+    return response
 
 
-@app.template_filter("last_update")
-def last_update(ingest):
-    try:
-        last_event = max(ingest["events"], key=lambda ev: ev["createdDate"])
-    except IndexError:
-        return ""
+@app.route('/')
+def homepage():
+    """Displays the homepage."""
+    return render_template('index.html')
+    
+@app.route('/dreams', methods=['GET', 'POST'])
+def dreams():
+    """Simple API endpoint for dreams. 
+    In memory, ephemeral, like real dreams.
+    """
+  
+    # Add a dream to the in-memory database, if given. 
+    if 'dream' in request.args:
+        DREAMS.append(request.args['dream'])
+    
+    # Return the list of remembered dreams. 
+    return jsonify(DREAMS)
 
-    last_event_date = last_event["createdDate"]
-
-    delta = dt.datetime.utcnow() - dt.datetime.strptime(
-        last_event_date, "%Y-%m-%dT%H:%M:%S.%fZ"
-    )
-
-    last_event_date_string = format_date(last_event_date)
-
-    if delta.seconds < 5:
-        return "%s (just now)" % last_event_date_string
-    elif delta.seconds < 60:
-        return "%s (%d seconds ago)" % (last_event_date_string, delta.seconds)
-    elif 60 <= delta.seconds < 120:
-        return "%s (1 minute ago)" % last_event_date_string
-    elif delta.seconds < 60 * 60:
-        return "%s (%d minutes ago)" % (last_event_date_string, int(delta.seconds / 60))
-    else:
-        return last_event_date_string
-
-
-@app.template_filter("format_date")
-def format_date(date_string):
-    if not date_string:
-        return ""
-
-    d = dt.datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
-
-    if d.date() == dt.datetime.now().date():
-        return d.strftime("today @ %H:%M")
-    elif d.date() == (dt.datetime.now() - dt.timedelta(days=1)).date():
-        return d.strftime("yesterday @ %H:%M")
-    else:
-        return d.strftime("%Y-%m-%d @ %H:%M")
-
-
-@app.route("/ingests/<ingest_id>")
-def lookup_ingest(ingest_id):
-    try:
-        ingest = prod_api.get_ingest(ingest_id=ingest_id)
-    except IngestNotFound:
-        try:
-            ingest = staging_api.get_ingest(ingest_id=ingest_id)
-        except IngestNotFound:
-            return render_template(
-                "not_found.html",
-                title=f"Could not find {ingest_id}",
-                ingest_id=ingest_id
-            )
-        except Exception as err:
-            print(f"Looking up {ingest_id}: {err}")
-            return render_template(
-                "error.html",
-                title=f"Error looking up {ingest_id}",
-                ingest_id=ingest_id
-            )
-        else:
-            return render_template(
-                "ingest.html",
-                title=f"Ingest {ingest_id}",
-                ingest=ingest,
-                api="staging"
-            )
-
-    except Exception:
-        print(f"Looking up {ingest_id}: {err}")
-        return render_template(
-            "error.html",
-            title=f"Error looking up {ingest_id}",
-            ingest_id=ingest_id
-        )
-
-    else:
-        return render_template(
-            "ingest.html",
-            title=f"Ingest {ingest_id}",
-            ingest=ingest,
-            api="production"
-        )
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
