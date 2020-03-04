852070c72f5fbcadc1d9c07aabb38a862db877a7
Alex Chan <alex@alexwlchan.net>
2020-03-04 23:31:34 +0000

    Record the ten most recent ingests that somebody looked at

--- static/ingest_inspector.js
+++ static/ingest_inspector.js
@@ -1,4 +1,66 @@
 function load_ingest() {
-  var ingestId = document.getElementById("ingestId").value;
+  const ingestId = document.getElementById("ingestId").value;
   document.location.href = "/ingests/" + ingestId;
+}
+
+function getRecentIngests() {
+  const storedIngests = localStorage.getItem("recentIngests");
+
+  if (storedIngests === null) {
+    return [];
+  }
+
+  return JSON.parse(storedIngests);
+}
+
+function storeNewIngest(ingestId, space, externalIdentifier) {
+  const recentIngests = getRecentIngests();
+
+  const otherIngests = recentIngests.filter(
+    ingest => ingest["ingestId"] != ingestId
+  );
+
+  const newIngests = [
+    {
+      "ingestId": ingestId,
+      "space": space,
+      "externalIdentifier": externalIdentifier
+    }
+  ].concat(otherIngests);
+
+  const ingestsToStore = newIngests.slice(0, 10);
+
+  localStorage.setItem("recentIngests", JSON.stringify(ingestsToStore));
+}
+
+function renderRecentIngestsList() {
+  const recentIngests = getRecentIngests();
+
+  console.log(recentIngests);
+
+  if (recentIngests !== []) {
+
+    recentIngestsDiv = document.getElementById("recentIngests");
+
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = "Recently viewed ingests:";
+    recentIngestsDiv.appendChild(paragraph);
+
+    var ingestList = document.createElement("ul");
+
+    for (var i = 0; i < recentIngests.length; i++) {
+      var ingest = recentIngests[i];
+
+      var listItem = document.createElement("li");
+      var link = document.createElement("a");
+      link.href = "/ingests/" + ingest["ingestId"];
+      link.innerHTML = ingest["ingestId"];
+
+      listItem.appendChild(link);
+      listItem.innerHTML += " &ndash; " + ingest["space"] + " / " + ingest["externalIdentifier"];
+      ingestList.appendChild(listItem);
+    }
+
+    recentIngestsDiv.appendChild(ingestList);
+  }
 }
